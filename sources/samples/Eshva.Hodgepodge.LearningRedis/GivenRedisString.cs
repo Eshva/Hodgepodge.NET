32394652aa3a0401b6db487f43176199d0436513
Mike Eshva <reg@eshva.ru>
2019-12-07 21:56:09 +0300

    hpn-008 add a sample project Learning Redis #008

---
+++ sources/samples/Eshva.Hodgepodge.LearningRedis/GivenRedisString.cs
@@ -0,0 +1,17 @@
+#region Usings
+
+using Xunit;
+
+#endregion
+
+
+namespace Eshva.Hodgepodge.LearningRedis
+{
+    public sealed class GivenRedisString
+    {
+        [Fact]
+        public void ShouldWriteString()
+        {
+        }
+    }
+}
