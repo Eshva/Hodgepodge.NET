dfaf388be3abb77d0fda097e9e2933e3d5b7a565
Mike Eshva <reg@eshva.ru>
2019-12-28 18:10:24 +0300

    hpn #044 bootstrap poll results service

---
+++ samples/Eshva.Hodgepodge.Polls/modules/Eshva.Polls.PollResults/Eshva.Polls.PollResults.WebApp/PollsPollResultsWebAppAssembly.cs
@@ -0,0 +1,14 @@
+#region Usings
+
+using System.Reflection;
+
+#endregion
+
+
+namespace Eshva.Polls.PollResults.WebApp
+{
+    public static class PollsPollResultsWebAppAssembly
+    {
+        public static readonly Assembly Reference = Assembly.GetExecutingAssembly();
+    }
+}
